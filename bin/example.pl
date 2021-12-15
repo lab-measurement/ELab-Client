50297784c60844190946151675d8b3cda300fc7f
Andreas K. Hüttel <dilfridge@gentoo.org>
2021-12-15 05:49:18 +0100

    Start filling in methods

---
+++ bin/example.pl
@@ -0,0 +1,8 @@
+
+use eLab::Client;
+ 
+
+my $elab = eLab::Client->new(
+	host => 'https://elab.somewhere.org/',
+	token => 12345,
+);
--- lib/eLab/Client.pm
+++ lib/eLab/Client.pm
@@ -6,13 +6,73 @@ use warnings;
 
 use Moose;
 use MooseX::NonMoose;
+use MooseX::Params::Validate;
 
-extends 'REST-Client';
+extends 'REST::Client';
+
+has token => (
+  is => 'ro',
+  isa => 'Str',
+  required => 1,
+);
+
+has endpoint => (
+  is => 'ro',
+  isa => 'Str',
+  default => 'api/v1/'
+);
+
+sub BUILD {
+  my $self = shift;
+  my $args = shift;
+
+  $self->addHeader('Authorization', $self->token());
+}
+
+sub elab_get {
+  my $self = shift;
+  my $url = shift;
+
+  return $self->GET($self->endpoint().$url);
+}
+
+sub elab_post {
+  my $self = shift;
+  my $url = shift;
+
+  return $self->POST($self->endpoint().$url);
+}
+
+
+# from here on we try to follow elabapy
+
+sub create_experiment {
+  my $self = shift;
+
+  return $self->elab_post("experiments");
+}
+
+sub create_item {
+  my $self = shift;
+  my $type = shift;
+
+  return $self->elab_post("items/".$type);
+}
+
+sub create_template {
+  my $self = shift;
+
+  return $self->elab_post("templates");
+}
 
 
 
+sub get_experiment {
+  my $self = shift;
+  my $id = shift;
 
-
+  return $self->elab_get("experiments/".$id);
+}
 
 
 no Moose;
